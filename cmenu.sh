1cdfbb536c0c495dd30ce3142d1d6596770f3228
0xdanelia <77199052+0xdanelia@users.noreply.github.com>
2021-02-16 15:25:31 -0500

    Fixed issue when the menu had no items in it.

--- cmenu.sh
+++ cmenu.sh
@@ -19,76 +19,86 @@ SEARCH_TEXT=''
 loop=true
 while $loop
 do
-# clear screen from cursor downward
-prs '\e[0J' 
-CURRENT_IDX=0  # index of current parameter
-PREV_IDX=0     # index of previous parameter
-MENU_PREV=0    # previous index from selected on-screen menu item
-MENU_NEXT=0    # following index from selected on-screen menu item
-NUM_ITEMS=0    # total items in on-screen menu
+	# clear screen from cursor downward
+	prs '\e[0J' 
+	CURRENT_IDX=0  # index of current parameter
+	PREV_IDX=0     # index of previous parameter
+	MENU_PREV=0    # previous index from selected on-screen menu item
+	MENU_NEXT=0    # following index from selected on-screen menu item
+	NUM_ITEMS=0    # total items in on-screen menu
 
-# loop through each parameter
-for ITEM in "$@"
-do	
-	let 'CURRENT_IDX+=1'
-	# check if current parameter matches the search string
-	if [[ -z $SEARCH_TEXT ||  $(echo "$ITEM" | grep "$SEARCH_TEXT") ]];
+	# loop through each parameter
+	for ITEM in "$@"
+	do	
+		let 'CURRENT_IDX+=1'
+		# check if current parameter matches the search string
+		if [[ -z $SEARCH_TEXT ||  $(echo "$ITEM" | grep "$SEARCH_TEXT") ]];
+		then
+			let 'NUM_ITEMS+=1'
+
+			# this will trigger one line after printing the selected row
+			if [[ $MENU_NEXT == -1 ]];
+			then
+				MENU_NEXT=$CURRENT_IDX
+			fi
+
+			# start new line for this menu item
+			prs '\n'
+			
+			# check if this item is currently selected
+			if [[ $CURRENT_IDX == $SELECTED_IDX ]];
+			then
+				# set highlighted color
+				prs '%b' $SELECTED_COLOR
+				# set some useful indexes centered around the selected item
+				SELECTED_ITEM=$ITEM
+				MENU_IDX=$NUM_ITEMS
+				MENU_PREV=$PREV_IDX
+				# this will set MENU_NEXT when printing the following row
+				MENU_NEXT=-1
+			fi
+			# print menu item
+			prs '%s' $ITEM
+			# reset color to default
+			prs '%b' $DEFAULT_COLOR
+			# the current values are saved as the previous values
+			PREV_ITEM=$ITEM
+			PREV_IDX=$CURRENT_IDX
+		else
+			# if the argument does not match the search string
+			if [[ $CURRENT_IDX == $SELECTED_IDX ]];
+			then
+				# if the selected item no longer matches an updated search string, select the next available item
+				let 'SELECTED_IDX+=1'
+			fi
+		fi
+	done
+
+	# check if the menu has no items
+	if [[ $NUM_ITEMS == 0 ]];
 	then
-		let 'NUM_ITEMS+=1'
-
-		# this will trigger one line after printing the selected row
-		if [[ $MENU_NEXT == -1 ]];
-		then
-			MENU_NEXT=$CURRENT_IDX
-		fi
-
-		# start new line for this menu item
-		prs '\n'
-		
-		# check if this item is currently selected
-		if [[ $CURRENT_IDX == $SELECTED_IDX ]];
-		then
-			# set highlighted color
-			prs '%b' $SELECTED_COLOR
-			# set some useful indexes centered around the selected item
-			SELECTED_ITEM=$ITEM
-			MENU_IDX=$NUM_ITEMS
-			MENU_PREV=$PREV_IDX
-			# this will set MENU_NEXT when printing the following row
-			MENU_NEXT=-1
-		fi
-		# print menu item
-		prs '%s' $ITEM
-		# reset color to default
-		prs '%b' $DEFAULT_COLOR
-		# the current values are saved as the previous values
-		PREV_ITEM=$ITEM
-		PREV_IDX=$CURRENT_IDX
-	else
-		# if the argument does not match the search string
-		if [[ $CURRENT_IDX == $SELECTED_IDX ]];
-		then
-			# if the selected item no longer matches an updated search string, select the next available item
-			let 'SELECTED_IDX+=1'
-		fi
+		SELECTED_ITEM=''
+		SELECTED_IDX=1
+		MENU_IDX=1
+	# if the selected row is now larger than the total rows after an updated search string, select the last row
+	elif [[ $SELECTED_IDX > $NUM_ITEMS ]];
+	then
+		SELECTED_ITEM=$PREV_ITEM
+		SELECTED_IDX=$PREV_IDX
+		MENU_IDX=$NUM_ITEMS
+		MENU_NEXT=0
+		# need to redraw the selected row, but now with highlighting
+		prs '\r%b%s%b' $SELECTED_COLOR $SELECTED_ITEM $DEFAULT_COLOR
 	fi
-done
-
-# if the selected row is now larger than the total rows after an updated search string, select the last row
-if [[ $SELECTED_IDX > $NUM_ITEMS ]];
-then
-	SELECTED_ITEM=$PREV_ITEM
-	SELECTED_IDX=$PREV_IDX
-	MENU_IDX=$NUM_ITEMS
-	MENU_NEXT=0
-	# need to redraw the selected row, but now with highlighting
-	prs '\r%b%s%b' $SELECTED_COLOR $SELECTED_ITEM $DEFAULT_COLOR
-fi
 
 	# move cursor back to top of list
-	prs '\e[%iA\r' $NUM_ITEMS
+	if [[ $NUM_ITEMS > 0 ]];
+	then
+		prs '\e[%iA' $NUM_ITEMS
+	fi
+	
 	# print user-typed text for searching list
-	prs '%s\e[K' $SEARCH_TEXT
+	prs '\r%s\e[K' $SEARCH_TEXT
 
 	# wait for user to hit a key
 	read -sn1 key
